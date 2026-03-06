eedc1c65a5ba04f2f5336cbc33128bec6fe2041e
Ahad Hasan <aha174862@gmail.com>
2026-03-07 01:07:50 +0600

    Refactor resend_email_otp.php for better clarity

--- book_swap_api/email_update/resend_email_otp.php
+++ book_swap_api/email_update/resend_email_otp.php
@@ -114,7 +114,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 $mail->SMTPAuth   = true;
                 $mail->Username   = 'bookswapcommunity@gmail.com';
                 //Enter your 16 digit mail password to send email
-                $mail->Password   = 'tqya kzyt lqnt uuxg';
+                $mail->Password   = 'your mail password';
                 $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
                 $mail->Port       = 587;
                 
@@ -160,4 +160,5 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 if (isset($connectNow)) {
     $connectNow->close();
 }
+
 ?>
